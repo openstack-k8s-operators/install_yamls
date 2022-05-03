f3a2ee2b75880e6a045036d51bdc11e451cdb823
Dan Prince <dprince@redhat.com>
2022-05-03 07:56:03 -0400

    Remove node/ from node name. Fixes scheduling issues

--- scripts/gen-crc-pv-kustomize.sh
+++ scripts/gen-crc-pv-kustomize.sh
@@ -4,7 +4,7 @@ if [ ! -d out ]; then
   mkdir -p out/crc
 fi
 
-NODE_NAME=$(oc get node -o name)
+NODE_NAME=$(oc get node -o name | sed -e 's|node/||')
 if [ -z "$NODE_NAME" ]; then
   echo "Unable to determine node name with 'oc' command."
   exit 1
