c366c0cd98c24555e76321dbefdb0c27da3e9621
Chandan Kumar <raukadah@gmail.com>
2023-02-02 17:12:10 +0530

    Use Kustomize to update openstack containers urls
    In order to use containers from different registeries with different
    hashes, we needed a way to update the OpenstackControlPlane CR file.
    
    This patch uses kustomize to achieve the same.
    
    Signed-off-by: Chandan Kumar <raukadah@gmail.com>

---
+++ scripts/gen-service-container-kustomize.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+#
+# Copyright 2022 Red Hat Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+set -ex
+
+# expect that the common.sh is in the same dir as the calling script
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+. ${SCRIPTPATH}/common.sh --source-only
+
+if [ ! -d ${DEPLOY_DIR} ]; then
+      mkdir -p ${DEPLOY_DIR}
+fi
+
+pushd ${DEPLOY_DIR}
+
+cat <<EOF >kustomization.yaml
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+resources:
+namespace: ${NAMESPACE}
+patches:
+- target:
+    kind: ${KIND}
+  patch: |-
+    - op: replace
+      path: /spec/keystone/template/containerImage
+      value: ${KEYSTONEAPI_IMG}
+    - op: replace
+      path: /spec/mariadb/template/containerImage
+      value: ${MARIADB_DEPL_IMG}
+    - op: replace
+      path: /spec/placement/template/containerImage
+      value: ${PLACEMENTAPI_IMG}
+    - op: replace
+      path: /spec/glance/template/containerImage
+      value: ${GLANCEAPI_IMG}
+    - op: replace
+      path: /spec/glance/template/glanceAPIInternal/containerImage
+      value: ${GLANCEAPI_IMG}
+    - op: replace
+      path: /spec/glance/template/glanceAPIExternal/containerImage
+      value: ${GLANCEAPI_IMG}
+    - op: replace
+      path: /spec/cinder/template/cinderAPI/containerImage
+      value: ${CINDERAPI_IMG}
+    - op: replace
+      path: /spec/cinder/template/cinderScheduler/containerImage
+      value: ${CINDERSCHEDULER_IMG}
+    - op: replace
+      path: /spec/cinder/template/cinderBackup/containerImage
+      value: ${CINDERBACKUP_IMG}
+    - op: replace
+      path: /spec/cinder/template/cinderVolumes/volume1/containerImage
+      value: ${CINDERVOLUME_IMG}
+    - op: replace
+      path: /spec/ovn/template/ovnDBCluster/ovndbcluster-nb/containerImage
+      value: ${OVNBDS_IMG}
+    - op: replace
+      path: /spec/ovn/template/ovnDBCluster/ovndbcluster-sb/containerImage
+      value: ${OVSBDS_IMG}
+    - op: replace
+      path: /spec/ovn/template/ovnNorthd/containerImage
+      value: ${OVNNORTHD_IMG}
+    - op: replace
+      path: /spec/ovs/template/ovsContainerImage
+      value: ${OVSSERVICE_IMG}
+    - op: replace
+      path: /spec/ovs/template/ovnContainerImage
+      value: ${OVNCONTROLLER_IMG}
+    - op: replace
+      path: /spec/neutron/template/containerImage
+      value: ${NEUTRONSERVER_IMG}
+EOF
+
+kustomization_add_resources
+
+popd
