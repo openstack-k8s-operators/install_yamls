833ff7348126ba40cbf45fb6fadf554ce111946d
Jiri Podivin <jpodivin@redhat.com>
2023-06-26 07:34:06 +0200

    Change default error handling
    Signed-off-by: Jiri Podivin <jpodivin@redhat.com>

--- devsetup/scripts/openstack-operator-cleanup.sh
+++ devsetup/scripts/openstack-operator-cleanup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+set -xe
+
 echo Openstack operator and crc cleanup
 echo ==================================
 
 cd ..
-make openstack_cleanup && \
-make ceph_cleanup && \
-make crc_storage_cleanup && \
-echo Environment teardown completed successfuly || \
-echo Environment teardown failed !
+make openstack_cleanup
+make ceph_cleanup
+make crc_storage_cleanup
+echo Environment teardown completed successfuly
 echo ==========================================
