780b436d2f50e1de5ce0aa59b6499ae7a1070602
Daniel Pawlik <dpawlik@redhat.com>
2024-04-11 09:00:16 +0200

    Drop support for 4.10 for certmanager; switch certmanager to stable-v1
    There is no need to check if the OCP release is 4.10, because supported
    versions are newer than 4.10.
    Also switch channel to stable-v1 [1] instead of using strict version.
    
    [1] https://docs.openshift.com/container-platform/4.15/security/cert_manager_operator/cert-manager-operator-install.html#cert-manager-operator-update-channels_cert-manager-operator-install
    
    Signed-off-by: Daniel Pawlik <dpawlik@redhat.com>

--- scripts/gen-olm-cert-manager.sh
+++ scripts/gen-olm-cert-manager.sh
@@ -23,10 +23,6 @@ if [ -z "${OPERATOR_NAMESPACE}" ]; then
     echo "Please set OPERATOR_NAMESPACE"; exit 1
 fi
 
-if [ -z "${OCP_RELEASE}" ]; then
-    echo "Please set OCP_RELEASE"; exit 1
-fi
-
 if [ -z "${CHANNEL}" ]; then
     echo "Please set CHANNEL"; exit 1
 fi
@@ -35,24 +31,10 @@ if [ ! -d ${OPERATOR_DIR} ]; then
     mkdir -p ${OPERATOR_DIR}
 fi
 
-echo OCP_RELEASE ${OCP_RELEASE}
 echo OPERATOR_DIR ${OPERATOR_DIR}
 echo OPERATOR_NAMESPACE ${OPERATOR_NAMESPACE}
 echo CHANNEL ${CHANNEL}
 
-if [ "$OCP_RELEASE" = "4.10" ]; then
-cat > ${OPERATOR_DIR}/operatorgroup.yaml <<EOF_CAT
-apiVersion: operators.coreos.com/v1
-kind: OperatorGroup
-metadata:
-  annotations:
-    olm.providedAPIs: CertManager.v1alpha1.config.openshift.io,CertManager.v1alpha1.operator.openshift.io,Certificate.v1.cert-manager.io,CertificateRequest.v1.cert-manager.io,Challenge.v1.acme.cert-manager.io,ClusterIssuer.v1.cert-manager.io,Issuer.v1.cert-manager.io,Order.v1.acme.cert-manager.io
-  generateName: cert-manager-operator-
-  name: openshift-cert-manager-operator-nd6mt
-  namespace: ${OPERATOR_NAMESPACE}
-spec: {}
-EOF_CAT
-else
 cat > ${OPERATOR_DIR}/operatorgroup.yaml <<EOF_CAT
 apiVersion: operators.coreos.com/v1
 kind: OperatorGroup
@@ -67,7 +49,6 @@ spec:
   - ${OPERATOR_NAMESPACE}
   upgradeStrategy: Default
 EOF_CAT
-fi
 
 cat > ${OPERATOR_DIR}/subscription.yaml <<EOF_CAT
 apiVersion: operators.coreos.com/v1alpha1
