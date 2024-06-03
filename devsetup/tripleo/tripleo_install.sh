dd85cef7ec5b6f2b3515e7eaaf74482ec5eb3d41
jgilaber <jgilaber@redhat.com>
2024-06-03 16:54:45 +0200

    Allow setting ntp server for tripleo adoption
    Allow controlling the ntp server to use for the deployment, so we can
    change it downstream.

--- devsetup/tripleo/tripleo_install.sh
+++ devsetup/tripleo/tripleo_install.sh
@@ -72,7 +72,7 @@ fi
 openstack overcloud deploy --stack overcloud \
     --override-ansible-cfg /home/zuul/ansible_config.cfg --templates /usr/share/openstack-tripleo-heat-templates \
     --roles-file ${ROLES_FILE} -n /home/zuul/network_data.yaml --libvirt-type qemu \
-    --ntp-server 0.pool.ntp.org,1.pool.ntp.org,2.pool.ntp.org,3.pool.ntp.org \
+    --ntp-server ${NTP_SERVER} \
     --timeout 90 --overcloud-ssh-user zuul --deployed-server \
     -e /home/zuul/hostnamemap.yaml -e /usr/share/openstack-tripleo-heat-templates/environments/docker-ha.yaml \
     -e /home/zuul/containers-prepare-parameters.yaml -e /usr/share/openstack-tripleo-heat-templates/environments/podman.yaml \
