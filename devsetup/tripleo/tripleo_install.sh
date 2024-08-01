5f83337da54f69d44c152f98ca1e35851f9c9469
m <mandreou@redhat.com>
2024-08-01 14:40:03 +0300

    Add config-download-networker for networker multinode adoption CI
    As part of [1] adds the bits needed for the multinode OSP17 deployment
    with dedicated networker nodes.
    
    [1] https://issues.redhat.com/browse/OSPRH-8810

--- devsetup/tripleo/tripleo_install.sh
+++ devsetup/tripleo/tripleo_install.sh
@@ -21,20 +21,35 @@ source stackrc
 openstack overcloud network provision --output network_provision_out.yaml ./network_data.yaml
 openstack overcloud network vip provision --stack overcloud --output vips_provision_out.yaml ./vips_data.yaml
 
+# check if hostnamemap contains networkers
+networker_nodes="FALSE"
+config_download="config-download.yaml"
+if (grep -q networker hostnamemap.yaml); then
+    networker_nodes="TRUE"
+    config_download="config-download-networker.yaml"
+fi
+
 # update the config-download with proper overcloud hostnames
 control0=$(grep "overcloud-controller-0" hostnamemap.yaml  | awk '{print $2}')
 control1=$(grep "overcloud-controller-1" hostnamemap.yaml  | awk '{print $2}')
 control2=$(grep "overcloud-controller-2" hostnamemap.yaml  | awk '{print $2}')
+sed -i "s/controller-0/${control0}/" $config_download
+sed -i "s/controller-1/${control1}/" $config_download
+sed -i "s/controller-2/${control2}/" $config_download
 compute0=$(grep "overcloud-novacompute-0" hostnamemap.yaml  | awk '{print $2}')
 compute1=$(grep "overcloud-novacompute-1" hostnamemap.yaml  | awk '{print $2}')
-compute2=$(grep "overcloud-novacompute-2" hostnamemap.yaml  | awk '{print $2}')
+sed -i "s/compute-0/${compute0}/" $config_download
+sed -i "s/compute-1/${compute1}/" $config_download
 
-sed -i "s/controller-0/${control0}/" config-download.yaml
-sed -i "s/controller-1/${control1}/" config-download.yaml
-sed -i "s/controller-2/${control2}/" config-download.yaml
-sed -i "s/compute-0/${compute0}/" config-download.yaml
-sed -i "s/compute-1/${compute1}/" config-download.yaml
-sed -i "s/compute-2/${compute2}/" config-download.yaml
+if [ $networker_nodes == "FALSE" ]; then
+    compute2=$(grep "overcloud-novacompute-2" hostnamemap.yaml  | awk '{print $2}')
+    sed -i "s/compute-2/${compute2}/" $config_download
+elif [ $networker_nodes == "TRUE" ]; then
+    networker0=$(grep "overcloud-networker-0" hostnamemap.yaml  | awk '{print $2}')
+    networker1=$(grep "overcloud-networker-1" hostnamemap.yaml  | awk '{print $2}')
+    sed -i "s/networker-0/${networker0}/" $config_download
+    sed -i "s/networker-1/${networker1}/" $config_download
+fi
 
 if [ "$EDPM_COMPUTE_CEPH_ENABLED" == "true"  ] ; then
     # use default role name for ComputeHCI in hostnamemap
@@ -46,23 +61,23 @@ if [ "$EDPM_COMPUTE_CEPH_ENABLED" == "true"  ] ; then
     hostnamemap="$hostnamemap\r  ComputeHCIHostnameFormat: '%stackname%-computehci-%index%'"
     # insert hostnamemap contents into config-download.yaml, we need it to generate
     # the inventory for ceph deployment
-    sed -i "s/parameter_defaults:/${hostnamemap}/" config-download.yaml
+    sed -i "s/parameter_defaults:/${hostnamemap}/" $config_download
 
     # swap computes for compute hci
-    sed -i "s/::Compute::/::ComputeHCI::/" config-download.yaml
+    sed -i "s/::Compute::/::ComputeHCI::/" $config_download
     # add storage management port to compute hci nodes
     stg_line="OS::TripleO::ComputeHCI::Ports::StoragePort: /usr/share/openstack-tripleo-heat-templates/network/ports/deployed_storage.yaml"
     stg_mgmt_line="OS::TripleO::ComputeHCI::Ports::StorageMgmtPort: /usr/share/openstack-tripleo-heat-templates/network/ports/deployed_storage_mgmt.yaml"
-    sed -i "s#$stg_line#$stg_line\r  $stg_mgmt_line\r#" config-download.yaml
+    sed -i "s#$stg_line#$stg_line\r  $stg_mgmt_line\r#" $config_download
     # correct RoleCount var in overcloud_services
     sed -i "s/ComputeCount/ComputeHCICount/" overcloud_services.yaml
 fi
 # Remove any quotes e.g. "np10002"-ctlplane -> np10002-ctlplane
-sed -i 's/\"//g' config-download.yaml
+sed -i 's/\"//g' $config_download
 # re-add newlines
-sed -i "s/\r/\n/g" config-download.yaml
+sed -i "s/\r/\n/g" $config_download
 # remove empty lines
-sed -i "/^$/d" config-download.yaml
+sed -i "/^$/d" $config_download
 
 # Add Manila bits
 MANILA_ENABLED=${MANILA_ENABLED:-true}
@@ -94,6 +109,6 @@ openstack overcloud deploy --stack overcloud \
     -e /home/zuul/containers-prepare-parameters.yaml -e /usr/share/openstack-tripleo-heat-templates/environments/podman.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/low-memory-usage.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/debug.yaml --validation-warnings-fatal ${ENV_ARGS} ${CEPH_OVERCLOUD_ARGS} \
-    -e /home/zuul/overcloud_services.yaml -e /home/zuul/config-download.yaml \
+    -e /home/zuul/overcloud_services.yaml -e /home/zuul/$config_download \
     -e /home/zuul/vips_provision_out.yaml -e /home/zuul/network_provision_out.yaml --disable-validations --heat-type pod \
     --disable-protected-resource-types
